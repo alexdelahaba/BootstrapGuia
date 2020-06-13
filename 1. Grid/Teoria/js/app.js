50a0f8643a472a195067a4259d500959f72cf154
Alejandro Martínez De La Haba <alejandro.martinez@soprasteria.com>
2020-06-13 18:05:41 +0200

    1st commit

---
+++ 1. Grid/Teoria/js/app.js
@@ -0,0 +1,33 @@
+$('#ocultarBloque1').click(() => {
+  $('.bloque1').toggle();
+});
+$('#ocultarBloque2').click(() => {
+  $('.bloque2').toggle();
+});
+$('#ocultarBloque3').click(() => {
+  $('.bloque3').toggle();
+});
+$('#ocultarBloque4').click(() => {
+  $('.bloque4').toggle();
+});
+$('#ocultarBloque5').click(() => {
+  $('.bloque5').toggle();
+});
+$('#ocultarBloque6').click(() => {
+  $('.bloque6').toggle();
+});
+$('#ocultarBloque7').click(() => {
+  $('.bloque7').toggle();
+});
+$('#tablaTamanios').click(() => {
+  $('.tablaTamanios').toggle();
+});
+$(document).ready(function () {
+  $('.bloque1').hide();
+  $('.bloque2').hide();
+  $('.bloque3').hide();
+  $('.bloque4').hide();
+  $('.bloque5').hide();
+  $('.bloque6').hide();
+  $('.bloque7').hide();
+});
